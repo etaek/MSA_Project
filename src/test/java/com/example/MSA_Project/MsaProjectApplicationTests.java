c2b3143626ab5b32f21404af8db4c0b099658fa7
unknown <LeeEunTaek@DESKTOP-CVKB37C.lgcns.com>
2021-12-13 14:25:03 +0900

    Initial commit

---
+++ src/test/java/com/example/MSA_Project/MsaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSA_Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
